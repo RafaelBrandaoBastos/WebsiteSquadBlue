f25d8a66d19cf011c85f2c6442606f587dcf127a
Jean-Stefan <jeanss12@gmail.com>
2022-07-05 16:17:14 -0300

    Style: Set global font-size

--- globalStyles.jsx
+++ globalStyles.jsx
@@ -1,12 +1,22 @@
 import {createGlobalStyle} from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-	*{
+
+*{
 		box-sizing: border-box;
 		font-family: 'Nunito', sans-serif;
 		user-select: none;
-	}
-	body, html, #root{
+}
+
+html {       
+font-size: 62.5%; 
+}
+
+body {       
+font-size: 1.6rem;
+}
+
+body, html, #root{
 	height: 100%;
 	padding: 0;
 	margin: 0;
