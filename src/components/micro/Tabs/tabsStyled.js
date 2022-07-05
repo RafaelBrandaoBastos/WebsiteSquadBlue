f901087662059825b2cd5439c480a4a8fc2c37d2
Jean-Stefan <jeanss12@gmail.com>
2022-07-05 17:56:48 -0300

    Style: added tab styles

---
+++ src/components/micro/Tabs/tabsStyled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Tab = styled.button`
+    height: 4.4rem;
+    width: 17.16rem;
+    background: #ffffff;
+    font-size: 1.6rem;
+    color: #111111;
+    border: 0;
+    border-bottom: 2px solid #aaaaaa;
+    outline: 0;
+    cursor: pointer;
+
+    ${({active}) =>
+        active &&
+        `
+    border-bottom: 3px solid #074EE8;
+		color: #074EE8;
+    opacity: 1;
+  `}
+`;
+
+export const TabGroup = styled.div`
+    display: flex;
+    max-width: 51.5rem;
+`;
