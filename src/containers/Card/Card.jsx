cbbb3e73fd5540639a73acd777e5c6cd6882b120
WelbertJr <welbertjunior@live.com>
2022-07-05 18:27:54 -0300

    Feat: adding card component

---
+++ src/containers/Card/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {ContainerCard, CardStyled} from "./cardStyled";
+
+export const Card = ()=>{
+    
+    return(
+        <ContainerCard>
+             <CardStyled/>
+        </ContainerCard>
+    )
+}
+
---
+++ src/containers/Card/cardStyled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const CardStyled = styled.div`
+width: 61.7rem;
+height: 73.9rem;
+background: #FFFFFF;
+border-radius: 20px;
+`;
+
+export const ContainerCard = styled.div`
+min-height: 100vh;
+width:100vw;
+background: #E5E5E5;
+display: flex;
+align-items: center;
+justify-content: center;
+`;
