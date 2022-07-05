8ee40956e6d64342f308edc53385702bf32abb08
WelbertJr <welbertjunior@live.com>
2022-07-05 19:50:36 -0300

    Feat: adding title component

---
+++ src/components/micro/Title/titleStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export const Title = styled.h1`
+font-family: 'Inter';
+font-style: normal;
+font-weight: 600;
+font-size: 1.8rem;
+line-height: 2.4rem;
+color: #111111;
+margin-left: 3rem;
+margin-top: 5rem;
+
+@media (max-width: 600px){
+font-size: 1.3rem;
+margin-left: 8rem;
+margin-top: 10rem;
+}
+`;
