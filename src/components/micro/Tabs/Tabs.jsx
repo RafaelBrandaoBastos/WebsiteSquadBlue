aba9fc9f0c0f6f747193be54891aac37b403055b
Jean-Stefan <jeanss12@gmail.com>
2022-07-06 13:40:07 -0300

    Feat: added tabs logic

--- src/components/micro/Tabs/Tabs.jsx
+++ src/components/micro/Tabs/Tabs.jsx
@@ -1,23 +1,32 @@
-import {useState} from 'react';
+import {useContext} from 'react';
 import {Tab, TabGroup} from './tabsStyled';
+import {TabsContext} from '../../../contexts/TabsProvider';
 
 const options = ['Basic', 'Social', 'Certificates'];
 
 const Tabs = () => {
-    const [active, setActive] = useState(options[0]);
+    const [selectedTab, setSelectedTab] = useContext(TabsContext);
+    const handleToggleTab = (index) => {
+        setSelectedTab(index);
+    };
+
     return (
         <>
             <TabGroup>
-                {options.map((option) => (
+                {options.map((option, index) => (
                     <Tab
+                        disabled={selectedTab < index}
+                        className={selectedTab === index && 'active'}
                         key={option}
-                        active={active === option}
-                        onClick={() => setActive(option)}
+                        onClick={() => handleToggleTab(index)}
                     >
                         {option}
                     </Tab>
                 ))}
             </TabGroup>
+            {selectedTab === 0 && 'inserir componente Basic'}
+            {selectedTab === 1 && 'inserir componente Social'}
+            {selectedTab === 2 && 'inserir componente Certificates'}
         </>
     );
 };
