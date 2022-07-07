141d103754921b7a93a6b6c9066170043a4ad55d
WelbertJr <welbertjunior@live.com>
2022-07-07 20:26:06 -0300

    Feat: adding code from button more

--- src/components/micro/Button/Button.jsx
+++ src/components/micro/Button/Button.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { ButtonStyled, ContainerButton } from "./buttonStyled";
+import { ButtonNextStyled, ButtonMoreStyled } from "./buttonStyled";
+import {TbPlus} from "react-icons/tb";
+import {MdNavigateNext} from "react-icons/md";
 
 const Button = (props)=>{
+    if(props.name=="Next")
     return(
-    <ContainerButton>
-    <ButtonStyled name={props.name} type={props.type}>{props.name}</ButtonStyled>
-    </ContainerButton>
+    <ButtonNextStyled name={props.name} type={props.type}>{props.name}</ButtonNextStyled>
+    )
+
+    if(props.name=="More")
+    return(
+      <ButtonMoreStyled name={props.name} type={props.type}>
+        {props.name}
+        <TbPlus color={"white"} size={"2rem"} style={{position:"absolute", left: "10%"}}/>
+        <MdNavigateNext color="white" size="2.5rem" style={{position:"absolute", right: "8%"}} />
+      </ButtonMoreStyled>
     )
 }
 
