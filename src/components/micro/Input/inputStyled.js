971062288306d19add27875c3ed23ad5802d4fb8
Jean-Stefan <jeanss12@gmail.com>
2022-07-07 12:48:03 -0300

    Style: Fixed input styles

--- src/components/micro/Input/inputStyled.js
+++ src/components/micro/Input/inputStyled.js
@@ -1,22 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const InputGlobal = styled.input`
+    padding: 1.1rem 1.2rem;
     width: 100%;
-    border-style: solid;
-    border-width: 3px;
-    border-color: #AAAAAA;
+    border: 2px solid #aaaaaa;
     border-radius: 4px;
-    height: 40px;
-    
+    height: 4rem;
+    font-size: 1.6rem;
+
+    ::placeholder {
+        font-size: 1.6rem;
+    }
 `;
 export const Label = styled.label`
-    font-family: "Nunito";
-    font-style: "regular";
-    font-size: 1.6rem;
+    font-size: 1.4rem;
     color: #767676;
 `;
 export const Container = styled.div`
     outline-style: solid;
     outline-color: red;
-    
 `;
