b2a4346a22f1d1674e841c6b707d302247a35971
Rafael Brandão <rafaelbrandaobastos@gmail.com>
2022-07-07 14:09:03 -0300

    Chore: Style Button Next

--- src/components/containers/FormSocial/Form.jsx
+++ src/components/containers/FormSocial/Form.jsx
@@ -5,8 +5,7 @@ import {Form, ContainerEmailPhone, DayMonth, YearAge, ContainerEmail, ContainerP
 const FormBasic = ()=>{
     return( 
         <Form>
-                <Input width="100%" height = "40px" label = "Linkedin" type="text" placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/" />
-                <Input width="100%" height = "40px" label = "Github" type="text" placeholder="https://github.com/foobar" />
+                
         </Form> 
     )
 };
