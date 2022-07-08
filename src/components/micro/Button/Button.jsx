a839e4e4cd05918eb32fb4ecb8987a1c6ff9d210
WelbertJr <welbertjunior@live.com>
2022-07-08 12:39:30 -0300

    Feat:adding finish button style

--- src/components/micro/Button/Button.jsx
+++ src/components/micro/Button/Button.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { ButtonNextStyled, ButtonMoreStyled } from "./buttonStyled";
+import { ButtonNextStyled, ButtonMoreStyled, ButtonFinishStyled } from "./buttonStyled";
 import {TbPlus} from "react-icons/tb";
 import {MdNavigateNext} from "react-icons/md";
+import {TbCheck} from "react-icons/tb"
 
 const Button = (props)=>{
     if(props.name=="Next")
     return(
-    <ButtonNextStyled name={props.name} type={props.type}>{props.name}</ButtonNextStyled>
+      <ButtonNextStyled name={props.name} type={props.type}>{props.name}</ButtonNextStyled>
     )
 
     if(props.name=="More")
@@ -16,6 +17,11 @@ const Button = (props)=>{
         <TbPlus color={"white"} size={"2rem"} style={{position:"absolute", left: "10%"}}/>
         <MdNavigateNext color="white" size="2.5rem" style={{position:"absolute", right: "8%"}} />
       </ButtonMoreStyled>
+    )
+
+    if(props.name=="Finish")
+    return(
+      <ButtonFinishStyled name={props.name} type={props.type}> {props.name}</ButtonFinishStyled>
     )
 }
 
--- src/components/micro/Button/buttonStyled.js
+++ src/components/micro/Button/buttonStyled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import NextIcon from "../../../assets/nextIcon.svg";
+import CheckIcon from "../../../assets/checkIcon.svg";
 
 export const ButtonNextStyled = styled.button`
 width: 8.1rem;
@@ -36,6 +37,30 @@ justify-content: center;
 position:relative;
 color: #FFFFFF;
 border: none;
+
+&:hover {
+    cursor: pointer;
+    background-color: #20b7d8;
+}
+`;
+
+export const ButtonFinishStyled = styled.button`
+width: 9.1rem;
+height: 4rem;
+background: #074EE8;
+border-radius: 0.4rem;
+font-size: 1.7rem;
+font-weight: 5;
+display: flex;
+align-items: center;
+padding-left: 3.5rem;
+position:relative;
+color: #FFFFFF;
+border: none;
+background-image: url(${CheckIcon});
+background-repeat: no-repeat;
+background-position: 0.8em;
+background-size: contain;
 
 &:hover {
     cursor: pointer;
