12aac9118414efb040e314f7787f71228bd3e26d
Jean-Stefan <jeanss12@gmail.com>
2022-07-07 23:19:43 -0300

    Merge branch 'develop' into forms/social

--- src/components/micro/Button/buttonStyled.js
+++ src/components/micro/Button/buttonStyled.js
@@ -1,37 +1,43 @@
-import styled from "styled-components";
-import NextIcon from "../../../assets/nextIcon.svg";
+import styled from 'styled-components';
+import NextIcon from '../../../assets/nextIcon.svg';
 
-export const ContainerButton = styled.div`
-display:flex;
-align-items: flex-end;
-justify-content: flex-end;
-flex-direction: column;
-width: 100%;
-height: 7%;
+export const ButtonNextStyled = styled.button`
+    width: 8.1rem;
+    height: 4rem;
+    background: #074ee8;
+    border-radius: 0.4rem;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    border: none;
+    color: #ffffff;
+    padding-left: 0.8em;
+    background-image: url(${NextIcon});
+    background-repeat: no-repeat;
+    background-position: 3.5em;
+    background-size: contain;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #20b7d8;
+    }
 `;
 
-export const ButtonStyled = styled.button`
-width: 8.1rem;
-height: 4rem;
-background: #074EE8;
-border-radius: 0.4rem;
-font-family: 'Nunito';
-font-style: normal;
-font-weight: 400;
-font-size: 1.6rem;
-line-height: 1.8rem;
-display: flex;
-align-items: center;
-border: none;
-color: #FFFFFF;
-padding-left: 0.8em;
-background-image: url(${NextIcon});
-background-repeat: no-repeat;
-background-position: 3.5em;
-background-size: contain;
+export const ButtonMoreStyled = styled.button`
+    width: 10.9rem;
+    height: 4rem;
+    background: #074ee8;
+    border-radius: 0.4rem;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    color: #ffffff;
+    border: none;
 
-&:hover {
-    cursor: pointer;
-    background-color: #20b7d8;
-}
+    &:hover {
+        cursor: pointer;
+        background-color: #20b7d8;
+    }
 `;
--- src/components/micro/Tabs/Tabs.jsx
+++ src/components/micro/Tabs/Tabs.jsx
@@ -1,9 +1,8 @@
 import {useContext} from 'react';
 import {Tab, TabGroup} from './tabsStyled';
 import {TabsContext} from '../../../contexts/TabsProvider';
-import FormBasic from '../../containers/FormBasic/form';
-import FormSocial from '../../containers/FormSocial/form';
-
+import FormBasic from '../../containers/FormBasic/Form';
+import FormSocial from '../../containers/FormSocial/Form';
 
 const options = ['Basic', 'Social', 'Certificates'];
 
@@ -27,9 +26,9 @@ const Tabs = () => {
                     </Tab>
                 ))}
             </TabGroup>
-            {selectedTab === 0 && <FormBasic/>}
-            {selectedTab === 1 && <FormSocial/>}
-            {selectedTab === 2 && "a"}
+            {selectedTab === 0 && <FormBasic />}
+            {selectedTab === 1 && <FormSocial />}
+            {selectedTab === 2 && 'a'}
         </>
     );
 };
