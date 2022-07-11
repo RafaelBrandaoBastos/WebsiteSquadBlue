e5d0ab73562234327367e353c817403f0f2997bf
WelbertJr <welbertjunior@live.com>
2022-07-11 14:53:58 -0300

    Feat: adding style code from Container Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import TabsProvider from './contexts/TabsProvider';
 import Card from './components/containers/Card/Card';
+import Header from "./components/containers/Header/Header";
 
 function App() {
     return (
-      
             <TabsProvider>
+                <Header/>
                 <Card/>
             </TabsProvider>
     
