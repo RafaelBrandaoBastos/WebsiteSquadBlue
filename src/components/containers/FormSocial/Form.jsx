950e9440afadc32597c22eb2e68fb2804acc467d
Jean-Stefan <jeanss12@gmail.com>
2022-07-17 20:23:41 -0300

    Refactor: Validation imported from yupSchema

--- src/components/containers/FormSocial/Form.jsx
+++ src/components/containers/FormSocial/Form.jsx
@@ -1,32 +1,23 @@
 import React, {useContext, useEffect} from 'react';
-import Input from "../../micro/Input/Input"
-import {Form, ErrorMessage, ContainerButton, ContainerLinkedin, ContainerGithub} from "./formStyled.js"
-import * as yup from 'yup';
+import Input from '../../micro/Input/Input';
+import {
+    Form,
+    ErrorMessage,
+    ContainerButton,
+    ContainerLinkedin,
+    ContainerGithub,
+} from './formStyled.js';
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {TabsContext} from '../../../contexts/TabsProvider';
 import Button from '../../micro/Button/Button';
 import {UserDataContext} from '../../../contexts/UserDataProvider';
+import {formSocialSchema} from '../../../utils/yupSchema';
 
-
-const schema = yup
-    .object({
-        linkedin: yup
-            .string()
-            .required('Please enter your Linkedin account')
-            .matches(/^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)/, 'Linkedin Invalid'),
-        github: yup
-            .string()
-            .required('Please enter your Github account')
-            .matches(/^(?:https?:\/\/)?(?:www\.)?(?:github\.com)\/[a-zA-Zà-úÀ-Ú.,-_!@#$%&*´`;:|]/,'Github Invalid'),
-    })
-    .required();
-
-const FormSocial = ()=>{
-
+const FormSocial = () => {
     const [userData, setUserData] = useContext(UserDataContext);
     const [selectedTab, setSelectedTab] = useContext(TabsContext);
-    
+
     const {
         getValues,
         register,
@@ -34,7 +25,7 @@ const FormSocial = ()=>{
         watch,
         setValue,
         formState: {errors},
-    } = useForm({resolver: yupResolver(schema)});
+    } = useForm({resolver: yupResolver(formSocialSchema)});
 
     const onSubmit = (data) => {
         setSelectedTab(selectedTab + 1);
@@ -43,28 +34,29 @@ const FormSocial = ()=>{
     };
 
     const SetData = () => {
-        localStorage.setItem("StorageSocial", JSON.stringify(getValues()));
+        localStorage.setItem('StorageSocial', JSON.stringify(getValues()));
     };
 
-
     const GetData = () => {
-        if (localStorage.getItem("StorageSocial")) { 
-            const StorageData = JSON.parse(localStorage.getItem("StorageSocial"));
+        if (localStorage.getItem('StorageSocial')) {
+            const StorageData = JSON.parse(
+                localStorage.getItem('StorageSocial'),
+            );
             const keys = Object.keys(StorageData);
             keys.forEach((key) => {
-                setValue(key, StorageData[key])
-            })
+                setValue(key, StorageData[key]);
+            });
         }
-    }
+    };
 
     useEffect(() => {
-        GetData()
-        window.addEventListener('beforeunload', (SetData())); 
-        return() => {
-            window.removeEventListener('beforeunload', SetData());     
-        }
+        GetData();
+        window.addEventListener('beforeunload', SetData());
+        return () => {
+            window.removeEventListener('beforeunload', SetData());
+        };
     }, []);
-    
+
     useEffect(() => {
         if (userData) {
             const keys = Object.keys(userData);
@@ -74,34 +66,39 @@ const FormSocial = ()=>{
         }
     });
 
-    return( 
+    return (
         <Form onSubmit={handleSubmit(onSubmit)}>
             <ContainerLinkedin>
-                <Input 
-                width="100%" 
-                label = "Linkedin" 
-                type="text" 
-                placeholder="https://www.linkedin.com/in/foo-bar-3a0560104/"  
-                {...{register: register('linkedin')}}/>
-                <ErrorMessage style={{left: '80px'}}>{errors.linkedin?.message}</ErrorMessage> 
+                <Input
+                    width='100%'
+                    label='Linkedin'
+                    type='text'
+                    placeholder='https://www.linkedin.com/in/foo-bar-3a0560104/'
+                    {...{register: register('linkedin')}}
+                />
+                <ErrorMessage style={{left: '80px'}}>
+                    {errors.linkedin?.message}
+                </ErrorMessage>
             </ContainerLinkedin>
 
             <ContainerGithub>
-                <Input 
-                width="100%" 
-                label = "Github" 
-                type="text" 
-                placeholder="https://github.com/foobar"  
-                {...{register: register('github')}}/>
-                <ErrorMessage style={{left: '80px'}}>{errors.github?.message}</ErrorMessage>
+                <Input
+                    width='100%'
+                    label='Github'
+                    type='text'
+                    placeholder='https://github.com/foobar'
+                    {...{register: register('github')}}
+                />
+                <ErrorMessage style={{left: '80px'}}>
+                    {errors.github?.message}
+                </ErrorMessage>
             </ContainerGithub>
-            
+
             <ContainerButton>
                 <Button name='Next' type='submit' />
-            </ContainerButton>     
-        </Form> 
-    )
+            </ContainerButton>
+        </Form>
+    );
 };
-  
-export default FormSocial;
 
+export default FormSocial;
